dd387b02641647ee062c70a5627c5568ebf67cc2
clj <30364709@qq.com>
2018-04-17 15:44:15 +0800

    访问mongodb

--- src/main/java/com/foda/StanfordChineseNlpExample.java
+++ src/main/java/com/foda/StanfordChineseNlpExample.java
@@ -1,93 +1,65 @@
 package com.foda;
 
-import edu.stanford.nlp.io.IOUtils;
-import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
-import edu.stanford.nlp.simple.*;
-import edu.stanford.nlp.util.CoreMap;
-import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.trees.TreeCoreAnnotations;
-import edu.stanford.nlp.dcoref.CorefChain;
-import edu.stanford.nlp.dcoref.CorefCoreAnnotations;
+import java.io.File;
+import java.io.FileReader;
+import java.util.*;
+import java.util.logging.Filter;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
+import org.apache.commons.collections.map.HashedMap;
+import org.bson.Document;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Field;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.MongoCollection;
+
+import com.opencsv.CSVReader;
 
 public class StanfordChineseNlpExample
 {
-    public static void main( String[] args )
-    {
-        StanfordChineseNlpExample example = new StanfordChineseNlpExample();
-        try {
-            example.runChineseAnnotators();
-        }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
+    public static void main(String[] args) throws Exception {
+        StanfordNlp nlp =new StanfordNlp();
+
+        String text = "叶凌：　　经查，你隐瞒了在南平市建阳区莒口镇生态保护工作站工作的事实，存在提供虚假材料，以欺骗手段取得二级建造师注册证书的行为。本厅于2017年9月8日作出《福建省住房和城乡建设厅撤销行政许可告知书》（闽建许函〔2017〕92号），并于2017年9月20日送达给你，你在规定的期限内未向本厅提出书面陈述、申辩。　　根据《中华人民共和国行政许可法》第六十九条、第七十九条和《注册建造师管理规定》第三十四条规定，本厅决定撤销对你作出的二级建造师注册许可，自作出撤销决定之日";
+        //String text = "近日，青岛7岁男孩“小长江”随叔叔送快递在网络引发热议。14日，据青岛市市北区人民政府新闻办公室通报，14日，市北区有关部门已将小长江送至青岛市儿童福利院，并安排专人陪护。15日，将先行安排小长江入学读书。通报表示，经了解，小长江自称今年约7岁，父亲去世，母亲已改嫁。其父亲生前工友颜先生带小长江一起从老家枣庄来青务工，居住生活在徐州北路6号申通快递点。此外，市北区有关部门将赴小长江母亲所在地，与当地派出所及孩子母亲取得联系，依法就孩子监护及户口问题进行协商。在此之前，市北区将妥善安排好小长江的学习生活，确保孩子身心健康。据媒体此前报道，今年7岁的男孩小长江父亲过世，母亲改嫁，此前随父亲生前工友颜世芳在青岛打工，帮其送快递，每日能送30件。";
+        for (int i=0;i<3;i++) {
+            long startTime = System.currentTimeMillis();    //获取开始时间
+            nlp.runChineseAnnotators(text);
+            long endTime = System.currentTimeMillis();    //获取结束时间
+            System.out.println("程序运行时间：" + (endTime - startTime) + "ms");    //输出程序运行时
+            System.out.println("-----------------------------------");
         }
 
+        // 读取mongo数据并解析
+        /*
+        int index=1;
+        MongoDBJDBC mongoDBJDBC=new MongoDBJDBC("175.102.18.112",27018);
+        MongoCollection<Document> doc=mongoDBJDBC.getCollections("tongji_zjj","lda_sum_data");
+        for (Document document:doc.find().projection(
+                Projections.fields(
+                        Projections.include("html"),
+                        Projections.excludeId()))){
+            System.out.println(Integer.toString(index)+"\t"+document.get("html"));
+            example.runChineseAnnotators(document.get("html").toString());
+        }
+        */
     }
 
-    public void runChineseAnnotators() throws Exception{
-
-        String text = "孩提王国的童话光环不知何时被掷落在地，安徒生的天国花园大概早已沉入冰穹。"+
-                "我想走入那密集汹涌的人潮，只为寻找一双赤子透澄明净的眼睛。"+
-                "我为这年岁渐长，而心灵提前硬化的世界感到惶恐，我为这不再皓洁，蒙上尘埃的心灵之窗感到悲哀。";
-        Annotation document = new Annotation(text);
-
-        Properties props = new Properties();
-        props.load(IOUtils.readerFromString("./StanfordCoreNLP-chinese.properties"));
-
-        StanfordCoreNLP corenlp = new StanfordCoreNLP(props);
-        corenlp.annotate(document);
-
-        parserOutput(document);
-    }
-
-    public void parserOutput(Annotation document){
-        // these are all the sentences in this document
-        // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
-        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
-
-        for(CoreMap sentence: sentences) {
-            // traversing the words in the current sentence
-            // a CoreLabel is a CoreMap with additional token-specific methods
-            for (CoreLabel token: sentence.get(CoreAnnotations.TokensAnnotation.class)) {
-                // this is the text of the token
-                String word = token.get(CoreAnnotations.TextAnnotation.class);
-                // this is the POS tag of the token
-                String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
-                // this is the NER label of the token
-                String ne = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
-
-                System.out.println(word+"\t"+pos+"\t"+ne);
-//                System.out.println(word);
+    // 读取一个csv文件并解析语法
+    public void csvParse(StanfordNlp example) throws Exception{
+        //File file=new File("/Users/fodaai01/Downloads/chatdata.csv");
+        FileReader fileReader = new FileReader("/Users/fodaai01/Downloads/chatdata_utf_8.csv");
+        CSVReader csvReader = new CSVReader(fileReader);
+        csvReader.readNext();   // 跳过表头
+        List<String[]> list = csvReader.readAll();
+        for (String[] str : list) {
+            try {
+                System.out.println(str[1]);
+                example.runChineseAnnotators(str[1]);
+                System.out.println("--------------------------");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
-
-            // this is the parse tree of the current sentence
-            Tree tree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
-            System.out.println("语法树：");
-//            System.out.println(tree.toString());
-//            tree.indentedListPrint();
-//            tree.indentedXMLPrint();
-            tree.pennPrint();
-
-            // this is the Stanford dependency graph of the current sentence
-//            SemanticGraph dependencies = sentence.get(SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation.class);
-//            System.out.println("依存句法：");
-//            System.out.println(dependencies.toString());
         }
-
-        // This is the coreference link graph
-        // Each chain stores a set of mentions that link to each other,
-        // along with a method for getting the most representative mention
-        // Both sentence and token offsets start at 1!
-//        Map<Integer, CorefChain> graph = document.get(CorefCoreAnnotations.CorefChainAnnotation.class);
     }
 
 }
